825e9a7fad7a3f9264a1d978654fbce670efbe67
Raúl Almeida <raulalmeida.es@gmail.com>
2020-12-21 22:27:17 +0100

    primer commit

---
+++ oh-my-zsh/alias_custom.zsh
@@ -0,0 +1,23 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+
+# List directory contents
+alias lsa='lsd -lah'
+alias l='lsd -lah'
+alias lat='lsd -lah --tree'
+alias ll='lsd -lh'
+alias la='lsd -lAh'
+alias cdw='cd $HOME/Workspace'
+alias cdd='cd $HOME/Workspace/dosfarma/'
+
+# List docker
+alias dl='docker_list'
+alias dco='docker_connect'
+alias dp='docker_prune'
---
+++ symlinks/links.sh
@@ -0,0 +1,3 @@
+ln -s "$HOME/.dotfiles/oh-my-zsh/alias_custom.zsh" "$HOME/.oh-my-zsh/custom"
+
+ln -s "$HOME/.dotfiles/zsh/.zshrc" "$HOME/"
